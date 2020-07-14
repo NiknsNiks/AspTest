77c4c429a1af9ab7909cc88405026725cab9abc2
Niks Ertmanis <Niks.Ertmanis@edu.rtu.lv>
2020-07-14 19:22:17 +0300

    Added model and context

---
+++ AspNetCoreTest/AspNetCoreTest.DataAccess/PersonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreTest.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTest.DataAccess
+{
+    public class PersonDbContext: DbContext // = Database
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder) 
+        {
+            optionbuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Initial Catalog=PersonTestDb;Integrated Security=True;");
+        }
+    }
+}
